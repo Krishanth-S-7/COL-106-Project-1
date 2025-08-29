f5b5e98b88951ff0e0d8e9e3ba105a9e27bbb40e
Krishanth-S-7 <krishanthiitian@gmail.com>
2025-08-30 00:50:59 +0530

    added hashmaps and file class

--- classes.hpp
+++ classes.hpp
@@ -21,4 +21,59 @@ class TreeNode{
     }
 };
 
+class map{
+    private:
+        long long size = 10;
+        vector<pair<int,TreeNode*>> m;
+        long long currcap = 0;
+    public:
+        map(){
+            m.resize(size);
+        }
+        void insert(int k, TreeNode* v){
+            if(currcap==size){
+            m.resize(2*size);
+            size *= 2;
+            }
+            m[currcap] = {k,v};
+            currcap++;
+        }
+        int find(int k){
+            if(k<0 || k>=currcap){
+                return -1;
+            }
+            if(m[k].second==NULL){
+                return -1;
+            }
+            else{
+                return k;
+            }
+        }
+        TreeNode* get(int k){
+            return m[k].second;
+        }
+        void erase(int k){
+            if(k<0 || k>=currcap){
+                return;
+            }
+            m[k].second = NULL;
+        }
+};
+
+class file{
+    public:
+    TreeNode* root;
+    map versions;
+    TreeNode* active_version;
+    int total_versions = 0;
+    file(){
+        root = new TreeNode();
+        versions.insert(0,root);
+        active_version = root;
+        total_versions = 1;
+    }
+    ~file(){
+        delete root;
+    }
+};
 #endif
